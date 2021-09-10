5ec9ad81dfce1d85354b08bd055d1967daa211e4
Nv7 <junk4Nv7@gmail.com>
2021-09-09 20:31:37 -0700

    Change URL

--- host/test/testprog.go
+++ host/test/testprog.go
@@ -3,7 +3,7 @@ package main
 import "google.golang.org/grpc"
 
 func main() {
-	_, err := grpc.Dial("rpc.nv7haven.tk:49152", grpc.WithBlock(), grpc.WithInsecure())
+	_, err := grpc.Dial("localhost:49152", grpc.WithBlock(), grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
