8ff68f1b0a87f86fa731619cd55415f5791330d4
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-09-30 23:05:53 -0700

    Add intro

---
+++ remodrive/src/Intro.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import { intro } from "./stores";
+</script>
+
+<h1>RemoDrive</h1>
+<p class="lead">RemoDrive allows you to remotely control an FTC robot! Get more information at <a href="https://github.com/Technova-12611/RemoDrive">our GitHub repository</a>!</p>
+<btn class="btn btn-success btn-lg" on:click={() => {intro.set(false)}}>Drive!</btn>
---
+++ remodrive/src/RemoDrive.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Config from "./Config.svelte";
+import Instructions from "./Instructions.svelte";
+import { editing, keyboard } from "./stores";
+</script>
+
+{#if $editing}
+  <Config/>
+{:else}
+  {#if $keyboard}
+    <Instructions/>
+  {:else}
+    <h1>Keybinds</h1>
+
+    <p class="lead">An action on the gamepad is mirrored on the robot. Toggle the switch in the top-right to simulate a gamepad with the keyboard.</p>
+  {/if}
+{/if}
