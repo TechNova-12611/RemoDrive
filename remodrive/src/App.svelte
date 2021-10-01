b003121158a74d306ff4f426d68da55fe3fad3ad
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-09-30 22:54:17 -0700

    Add intro page

--- remodrive/src/App.svelte
+++ remodrive/src/App.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
-import Config from "./Config.svelte";
-import Instructions from "./Instructions.svelte";
+import Intro from "./Intro.svelte";
 import Navbar from "./Navbar.svelte";
-import { editing, keyboard } from "./stores";
+import RemoDrive from "./RemoDrive.svelte";
+import { intro} from "./stores";
 </script>
 
 <Navbar/>
 <div class="container text-center mt-3">
-	{#if $editing}
-		<Config/>
+	{#if $intro}
+		<Intro/>
 	{:else}
-		{#if $keyboard}
-			<Instructions/>
-		{:else}
-			<h1>Keybinds</h1>
-
-			<p class="lead">An action on the gamepad is mirrored on the robot. Toggle the switch in the top-right to simulate a gamepad with the keyboard.</p>
-		{/if}
+		<RemoDrive/>
 	{/if}
 </div>
--- remodrive/src/stores.ts
+++ remodrive/src/stores.ts
@@ -8,3 +8,4 @@ export const gamepad1 = "g1";
 export const gamepad2 = "g2";
 
 export const editing = writable(true);
+export const intro = writable(true);
